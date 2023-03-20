f5a1b9153968de3d19496a15d884b3d591327985
wagner <wagner.santos@accurate.com.br>
2023-03-20 11:07:10 -0300

    Declarando um inteiro

---
+++ aula_002_variaveis_e_fluxos/src/TestaVariaveis.java
@@ -0,0 +1,8 @@
+public class TestaVariaveis {
+    public static void main (String[] args){
+        int idade;
+        idade = 34;
+
+        System.out.println("Olá! Eu tenho " +  idade + " anos.");
+    }
+}
