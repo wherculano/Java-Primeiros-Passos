70abafedcec7bf003f2413df3db19f5916461da8
wagner <wagner.santos@accurate.com.br>
2023-03-21 10:50:09 -0300

    Exemplo laço For

---
+++ aula_002_variaveis_e_fluxos/src/TestaFor.java
@@ -0,0 +1,7 @@
+public class TestaFor {
+    public static void main(String[] args) {
+        for(int contador = 1; contador <= 10; contador++){
+            System.out.println(contador);
+        }
+    }
+}
