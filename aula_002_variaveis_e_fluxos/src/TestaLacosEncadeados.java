3b681dc9d1769d2a348cbd34fc68ac46cda2fb77
wagner <wagner.santos@accurate.com.br>
2023-03-21 10:58:07 -0300

    Exemplo de lacos encadeados: Tabuada

---
+++ aula_002_variaveis_e_fluxos/src/TestaLacosEncadeados.java
@@ -0,0 +1,11 @@
+public class TestaLacosEncadeados {
+    public static void main(String[] args) {
+        // Tabuada
+        for(int n1 = 1; n1 <=10; n1++){
+            for(int n2 = 1; n2<=10; n2++){
+                System.out.println(n1 + " x " + n2 + " = " + n1*n2);
+            }
+            System.out.println("---------------");
+        }
+    }
+}
