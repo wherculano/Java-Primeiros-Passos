46d118e1037fcc1c341045382f0815f32fb3632a
wagner <wagner.santos@accurate.com.br>
2023-03-21 00:18:33 -0300

    Exemplo de condicional

--- aula_002_variaveis_e_fluxos/src/TestaCondicional.java
+++ aula_002_variaveis_e_fluxos/src/TestaCondicional.java
@@ -2,17 +2,13 @@ public class TestaCondicional {
     public static void main(String[] args) {
         int idade = 20;
         int quantidadeDePessoas = 3;
+        boolean acompanhado = quantidadeDePessoas >= 2;
 
-        if(idade >= 18){
-            System.out.println("Voce tem mais de 18 anos!\nSeja bem vind!!o");
+        if(idade >= 18 || acompanhado){
+            System.out.println("Seja bem vindo!");
         }
         else{
-            if (quantidadeDePessoas >= 2) {
-                System.out.println("Você é meor de idade, mas acompanhado de um adulto você pode entrar!");
-            }
-            else{
-                System.out.println("Que pena! Você não pode entrar");
-            }
+            System.out.println("Que pena! Você não pode entrar");
         }
     }
 }
